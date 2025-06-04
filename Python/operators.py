0fc1f6da90744400a01a9d16468b8be0f79ce8f0
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-06-04 13:52:29 +0530

    New codes

---
+++ Python/operators.py
@@ -0,0 +1,27 @@
+num1 = 3
+num2 = 2
+sum = num1 + num2
+diff = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+remainder = num1 % num2
+ye_kya_hai = num1 ** num2
+print(sum ," ", diff ," ", product ," ", quotient ," ", remainder ," ", ye_kya_hai)
+#the last operatoer is used for exponents
+num3 = 3
+num3 += 2
+print(num3)
+num3 -= 2
+print(num3)
+num3 *= 2
+print(num3)
+num3 /= 2
+print(num3)
+num3 %= 2
+print(num3)
+num3 **= 2
+print(num3)
+"""
+the (operator)= are assigned operators
+this means the new value of num3 will be num3 (operator) 2..
+"""
