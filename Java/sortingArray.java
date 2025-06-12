ab7a36a329f693cd877ff3c8f6422288a5cb0759
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-06-12 20:16:37 +0530

    New codes

--- Java/sortingArray.java
+++ Java/sortingArray.java
@@ -2,19 +2,22 @@ public class sortingArray
 {
     public static void main(String[] args)
     {
-        int[] Array = {6, 3, 4, 10, 6};
+        int[] Array = {3, 6, 4, 10, 6};
         for(int i = 0; i < Array.length; i++)
         {
             for(int o = 0; o < Array.length-1; o++)
             {
-                if(Array[o] >= Array[o+1])
+                if(Array[o] <= Array[o+1])
                 {
-                    Array[o] = Array[o] + Array[o+1];
+                    Array[o] = Array[o] + Array[o+1]; 
                     Array[o+1] = Array[o] - Array[o+1];
                     Array[o] = Array[o] - Array[o+1];
                 }
             }
-            System.out.print(Array[i] + " ");
+        }
+        for(int A = 0; A <= Array.length - 1; A++)
+        {
+            System.out.print(Array[A] + " ");
         }
     }
 }
--- Python/sortingAnArray.py
+++ Python/sortingAnArray.py
@@ -1,8 +1,8 @@
-Array = [ 6, 3, 4, 10, 6]
-for i in range(0,4):
-    for i in range(0,4):
-        if(Array[i] <= Array[i+1]):
-            Array[i] = Array[i+1] + Array[i] #a = 10 // b = 5 | a = 15
-            Array[i+1] = Array[i] - Array[i+1] #b = 10
-            Array[i] = Array[i] - Array[i+1] #a = 5
+Array = [3, 6, 4 ,6, 10, 7]
+for i in range(0,len(Array) - 1):
+    for i in range(0,len(Array)-1):
+        if(Array[i] >= Array[i+1]):
+            Array[i] = Array[i] + Array[i+1]
+            Array[i+1] = Array[i] - Array[i+1]
+            Array[i] = Array[i] - Array[i+1]
 print(Array)
