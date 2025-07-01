867b645ca85dd6f732b1fdb38b82be79c9fce5eb
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-07-01 21:03:51 +0530

    new codes

--- Java/sortingArray.java
+++ Java/sortingArray.java
@@ -1,13 +1,15 @@
+import java.util.Random;
 public class sortingArray
 {
     public static void main(String[] args)
     {
-    int[] Array = {3, 6, 4, 10, 6};
+    Random ra = new Random();    
+    int[] Array = {ra.nextInt(100), ra.nextInt(100), ra.nextInt(100), ra.nextInt(100), ra.nextInt(100)};
         for(int i = 0; i < Array.length; i++)
         {
             for(int o = 0; o < Array.length-1; o++)
             {
-                if(Array[o] <= Array[o+1])
+                if(Array[o] >= Array[o+1])
                 {
                     Array[o] = Array[o] + Array[o+1]; 
                     Array[o+1] = Array[o] - Array[o+1];
