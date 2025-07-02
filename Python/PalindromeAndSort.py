8cd5216827d936d781c774f0650000e2e20d5fba
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-07-02 21:15:25 +0530

    new codes

--- Python/PalindromeAndSort.py
+++ Python/PalindromeAndSort.py
@@ -1,12 +1,10 @@
-array = [12, 39, 45, 56, 45, 39, 12]
-array2 = array.copy()
-array2.reverse()
-if(array == array2):
-    print(array, "is a palindrome.")
+word = input("Enter the word :")
+wordInput = word.lower()
+listInput = list(wordInput)
+palindromeCheck = []
+for i in range(0,len(word)):
+    palindromeCheck.append(wordInput[len(word) - 1 - i])
+if(palindromeCheck == listInput):
+    print("It is a palindrome")
 else:
-    print(array, "is not a palindrome")
-#Now this is another program to count and sort
-array3 = ["C", "D", "A", "A", "B", "B", "A"]
-print(array3.count('A'))
-array3.sort()
-print(array3)
+    print("It is not a palindrome")
