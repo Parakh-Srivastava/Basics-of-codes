75407bd2eb6224124dc45a2fc3aaaa958a16c93a
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-08-09 01:50:06 +0530

    OOP

---
+++ Python/IntroToClasses.py
@@ -0,0 +1,11 @@
+class Grove4Life():
+    def __init__(self, name,age):
+        self.name = name
+        self.age = age
+
+Leader = Grove4Life("Sweet", 32)
+coLeader = Grove4Life("CJ", 26)
+RightHand = Grove4Life("Ryder", 28)
+LeftHand = Grove4Life("Big Smoke", 30)
+
+print(Leader,"\n",coLeader,"\n", RightHand, "\n", LeftHand)
