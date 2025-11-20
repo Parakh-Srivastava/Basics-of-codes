293e305b9859006b3c3b4d296631e371908ec7bc
Parakh <parakhsriv01@gmail.com>
2025-11-20 05:59:08 +0530

    Update codes

--- C/pointers.c
+++ C/pointers.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+
+
+/*sabse pehle num1 ke address mein num2 ka value daalenge fir num2 ke adress mein num1 ka address maange*/
+void swapnum(int* num1, int* num2)
+{
+    int temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
+
 int main()
 {
-    /*Pointer is a kind of variable which stores the adress of other variables.*/
-    int age = 10;
-    int *pAge = &age;
-    printf("%d", *pAge);
-    return 0; 
+    int a = 12, b = 21;
+    swapnum(&a,&b);
+    printf("%d %d",a,b);
+    for(int i = 1; i <= 10; i++)
+    {
+        if(i == 7)
+        {
+            goto thala;
+        }
+        else
+            printf("%d\n", i);
+    }
+    return 0;
+    thala:
+    {
+        printf("thala for a reason \n");
+    }
 }
